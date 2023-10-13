e5955a8b692f559d1bc15eeed0b4c0b79f85fe2b
devhwaseop <devhwaseop@gmail.com>
2023-10-13 12:24:17 +0900

    mugunghwa_DEAD_Create

--- jjuggumi/mugunghwa.c
+++ jjuggumi/mugunghwa.c
@@ -57,7 +57,6 @@ void map_replace(char a, char b) {
 void m_move_random(int player, int dir) {
 	int p = player;  // 이름이 길어서...
 	int nx, ny;  // 움직여서 다음에 놓일 자리
-	int xy0 = 1;
 	
 	//if (dir == 1) { xy0 = nx == 0 && ny == 0; }
 	//else { xy0 = 1; }
@@ -76,73 +75,158 @@ void m_move_random(int player, int dir) {
 			}
 		}
 		else{
-			xy0 = nx == 0 && ny == 0;
 			nx = px[p] + randint(-1, 1);
-			ny = py[p] + randint(-1, 0);
-		}
-	} while (!placable(nx, ny) && xy0);
+			ny = py[p] + randint(-1, 0);}
+	} while (!placable(nx, ny) || ((dir == 1) ? (nx == 0 && ny == 0) : 0) );
 
 	move_tail(p, nx, ny);
 }
 
-void mugunghwa_string(int *y, int *print_time) {
-	int x = N_ROW;
-	if (*y == 30) {
-		for (int i = 3; i < n_player - 2 + 3; i++){
-			front_buf[i][1] = '@';
-		printxy(front_buf[i][1], i, 1);
+void m_erase(int *y, int *print_time) {
+	// "무궁화꽃이피었습니다" 지우기
+	for (int i = 0; i < 30; i += 3) {
+		gotoxy(N_ROW, i);
+		printf(" ");
+		gotoxy(N_ROW, i + 1);
+		printf(" ");
+	}
+	// y 초기화
+	*y = 0;
+	// 다음 "무"를 약 500ms 후 출력 
+	*print_time += 5;
+}
+
+// 움직인 플레이어를 true로 정의 후 배열로 반환
+int m_auto_player() {
+	//int *player_move[PLAYER_MAX] = { 0 };
+	for (int i = 1; i < n_player; i++) {
+		// player 1 부터는 1/10확률중 임의로 7이 랜덤으로 반환시 사망
+		//if (!player[i]) { player_move[i] = true; }
+		//else if (randint(0, 9) == 7) {
+		if (randint(0, 9) == 7) {
+			m_move_random(i, 1);
+			//player_move[i] = true;
 		}
+	}
+	//return player_move ;
+}
+void m_print(int *y, int *print_time) {
+	char m_g_h[50][4] = { {"무"},{"궁"},{"화"}, {"꽃"}, {"이"}, {"피"}, {"었"}, {"습"}, {"니"}, {"다"} }; // "무 궁 화 꽃 이 피 었 습 니 다";
+	gotoxy(N_ROW, *y);
+	if(*y == 0){ printf("%s", m_g_h[*y/3]); }
+	else { printf("%s", m_g_h[*y/3]); }
+
+	*y = *y + 3;
+	int a = 5, b = 3, c = randint(0, 1);
+	switch (*y) {
+	case 0: *print_time += randint(a, a += b + c); break;
+	case 3: *print_time += randint(a, a += b + c); break;
+	case 6: *print_time += randint(a, a += b + c); break;
+	case 9: *print_time += randint(a, a += b + c); break;
+	case 12: *print_time += randint(a, a += b + c); break;
+	case 15: *print_time += randint(a, a -= b + c); break;
+	case 18: *print_time += randint(a, a -= b + c); break;
+	case 21: *print_time += randint(a, a -= b + c); break;
+	case 24: *print_time += randint(a, a -= b + c); break;
+	case 27: *print_time += randint(a, a -= b + c); break;
+	}
+}
+
+int *player_overlap() {
+	int d_true[PLAYER_MAX] = { 0 };
+	int p;
+
+	for (int row = 1; row <= N_ROW-2; row++) {
+		for (int col = 1; col <= N_COL-2; col++) {
+			if (front_buf[row][col] != ' ' && front_buf[row][col] != '#' && front_buf[row][col] != '@') {
+				p = front_buf[row][col];
+				p = p - 48; // '0' = 48
+				d_true[p] = true;
+				break;
+			}
+		}
+	}
+	return d_true;
+}
+
+bool m_player_a_or_d() {
+	//int *p_move = m_auto_player();
+	int past_tick = tick;
+	int r_tick = randint(130, 250) * 10;
+	int past_x[PLAYER_MAX] = {0};
+	int past_y[PLAYER_MAX] = {0};
+	for (int i = 0; i < n_player; i++) {
+		past_x[i] = px[i];
+		past_y[i] = py[i];
+	}
+
+	bool p_0 = player[0];
+	while (tick != past_tick + 3000){
+		if (tick == r_tick * 10) { 
+			draw();
+		}
+
 		key_t key = get_key();
 		if (key == K_QUIT) {
-			return K_QUIT;
+			return false;
+		}
+		else if (p_0 && key != K_UNDEFINED) {
+			move_manual(key);
+			break;
+			draw();
+		}
+
+		Sleep(10);
+		tick += 10;
+	}
+	int *p_overlap = player_overlap();
+	int dead_now[PLAYER_MAX] = { 20, 20, 20, 20, 20, 20, 20, 20, 20, 20 };
+	int dn_i = 0;
+	for (int i = 0; i < n_player; i++) {
+		if (past_x[i] == px[i] && past_y[i] == py[i])
+			continue;
+		
+		if (p_overlap[i]) {
+			player[i] = false;
+			back_buf[px[i]][py[i]] = ' ';
+			dead_now[dn_i] = i;
+			dn_i += 1;
+		}
+	}
+	if (dn_i != 0) {
+		n_alive -= dn_i;
+		// 다이얼로그 출력 죽은사람 몇명 식으로 
+	}
+	return true; 
+}
+
+bool m_game(int *y, int *print_time) {
+	if (*y < 30 && tick >= (*print_time * 100)) {
+		m_print(y, print_time);
+	}
+	else if (*y == 30) {
+		// "#"을 "@"로 바꿈
+		for (int i = 3; i < n_player - 2 + 3; i++) {
+			front_buf[i][1] = '@';
+			printxy(front_buf[i][1], i, 1);
+		}
+
+		key_t key = get_key();
+		if (key == K_QUIT) {
+			return false;
 		}
 		else if (key != K_UNDEFINED) {
 			move_manual(key);
 		}
 
-		// player 1 부터는 랜덤으로 움직임(8방향)
-		for (int i = 1; i < n_player; i++) {
-			// player 1 부터는 1/10확률중 임의로 7이 랜덤으로 반환시 사망
-			if (randint(0, 9) == 7) {
-				m_move_random(i, 1);
-				//player[i] = false;
-			}
-		}
+		// 삶, 죽음 판정 코드
+		bool a_d = m_player_a_or_d();
+		*print_time = *print_time + 30;
 
-		// 삶과 죽음 판정 코드 생성예정
+		if (!a_d) { return false; }
 
-		// "무궁화꽃이피었습니다" 지우기
-		for (int i = 0; i < 30; i+=3) {
-			gotoxy(x, i);
-			printf(" ");
-			gotoxy(x, i+1);
-			printf(" ");
-		}
-		// y 초기화
-		*y = 0;
-		// 다음 "무"를 약 500ms 후 출력 
-		*print_time += 5;
-	}
-	if (*y < 30 && tick >= (*print_time * 100)) {
-		char m_g_h[50] = "무 궁 화 꽃 이 피 었 습 니 다";
-		gotoxy(x, *y);
-		printf("%c%c", m_g_h[*y], m_g_h[*y+1]);
-		
-		*y = *y + 3;
-		int a = 5, b = 3, c = randint(0, 1);
-		switch (*y) {
-		case 0: *print_time += randint(a, a += b +c); break;
-		case 3: *print_time += randint(a, a += b +c); break;
-		case 6: *print_time += randint(a, a += b +c); break;
-		case 9: *print_time += randint(a, a += b +c); break;
-		case 12: *print_time += randint(a, a += b +c); break;
-		case 15: *print_time += randint(a, a -= b +c); break;
-		case 18: *print_time += randint(a, a -= b +c); break;
-		case 21: *print_time += randint(a, a -= b +c); break;
-		case 24: *print_time += randint(a, a -= b +c); break;
-		case 27: *print_time += randint(a, a -= b +c); break;
-		}
-
+		m_erase(y, print_time);
+		return true;
 	}
 }
 
@@ -159,18 +243,19 @@ void mugunghwa(void) {
 		if (key == K_QUIT) {
 			break;
 		}
-		else if (key != K_UNDEFINED) {
+		else if (player[0] && key != K_UNDEFINED) {
 			move_manual(key);
 		}
 
 		// player 1 부터는 랜덤으로 움직임(8방향)
 		for (int i = 1; i < n_player; i++) {
-			if (tick % period[i] == 0) {
+			if (player[i] && tick % period[i] == 0) {
 				m_move_random(i, -1);
 			}
 		}
 
-		mugunghwa_string(&mgh_y, &print_time);
+		bool a_d = m_game(&mgh_y, &print_time);
+		if (!a_d) { break; }
 
 		display();
 		Sleep(10);
